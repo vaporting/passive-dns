4263fc8f70deee7c6f2535dfb20131622fe8c059
vaporting <freehotday@gmail.com>
2019-10-02 04:39:30 +0800

    add dns record types in types.go

--- types/types.go
+++ types/types.go
@@ -1,5 +1,15 @@
 package types
 
+// DNS record types
+const (
+	// DNSIpv4Type record type
+	DNSIpv4Type string = "A"
+	// DNSIpv6Type record type
+	DNSIpv6Type string = "AAAA"
+	// DNSDomainType record type
+	DNSDomainType string = "CNAME"
+)
+
 // ResolvedEntry is used to store the resolved entry from request
 type ResolvedEntry struct {
 	Name      string `json:"name"`
