804935d00bce9da0a2b285a1af6c9664048c1363
vaporting <freehotday@gmail.com>
2019-10-02 17:54:23 +0800

    change gorm to go-pg, due to the completion of ORM features
    modify InitDB, GetDB
    
    new func: CloseDB

--- db/db.go
+++ db/db.go
@@ -1,30 +1,41 @@
 package db
 
 import (
-	"github.com/jinzhu/gorm"
-
-	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/go-pg/pg"
 
 	"errors"
 )
 
-var passiveDB *gorm.DB
+var passiveDB *pg.DB
 
 // InitDB initializes the database only once
-func InitDB() (*gorm.DB, error) {
+func InitDB() (*pg.DB, error) {
 	var err error = nil
 	if passiveDB == nil {
-		passiveDB, err = gorm.Open("postgres", "user= dbname=passivedns_dev password=")
-		passiveDB.LogMode(true)
+		passiveDB = pg.Connect(&pg.Options{
+			User:     "",
+			Password: "",
+			Database: "passivedns_dev",
+		})
+		if _, err = passiveDB.Exec("SELECT 1;"); err != nil {
+			passiveDB = nil
+		}
 	}
 	return passiveDB, err
 }
 
 // GetDB get DB object, if DB is initialized
-func GetDB() (*gorm.DB, error) {
+func GetDB() (*pg.DB, error) {
 	err := errors.New("DB is uninitialized")
 	if passiveDB != nil {
 		err = nil
 	}
 	return passiveDB, err
+}
+
+// CloseDB closes DB
+func CloseDB() {
+	if passiveDB != nil {
+		passiveDB.Close()
+	}
 }
