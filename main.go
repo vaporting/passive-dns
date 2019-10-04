6e6fe7f2de1920fd8bfa970702177e294f1425ad
vaporting <freehotday@gmail.com>
2019-10-04 19:24:32 +0800

    modify main.go for passiv-dns first version

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
-import "passive-dns/routes"
+import (
+	"passive-dns/routes"
+
+	"fmt"
+
+	"passive-dns/db"
+)
 
 func main() {
+	db.InitDB()
+	_, err := db.GetDB()
+	fmt.Println(err)
 	r := routes.InitRoutes()
 	r.Run(":8080") // listen and serve on 0.0.0.0:8080
 }
