ce7314b7f6f382a188797b0ddf9e9f2048139b1a
vaporting <freehotday@gmail.com>
2019-10-04 19:16:40 +0800

    add Dname for getting domain:name during join query

--- models/resolved_ip.go
+++ models/resolved_ip.go
@@ -5,9 +5,13 @@ import "time"
 // ResolvedIP is the table:Resolved_ips template
 type ResolvedIP struct {
 	BaseModel
-	DomainID uint
+	DomainID     uint
 	ResolvedIpID uint
-	SourceID uint
-	FirstSeen time.Time
-	LastSeen time.Time
+	SourceID     uint
+	FirstSeen    time.Time
+	LastSeen     time.Time
+
+	// relations
+	// Dname is relative to table:Domains:name
+	Dname string
 }
