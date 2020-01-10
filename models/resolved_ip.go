fc8ce17f4514266ea61093b12f937fec7340c69b
vaporting <freehotday@gmail.com>
2020-01-10 18:45:56 +0800

    add new model of resovled_ip joining with table:domain, ip
    for synchronous service uses
    postgresql -> redis

--- models/resolved_ip.go
+++ models/resolved_ip.go
@@ -15,3 +15,19 @@ type ResolvedIP struct {
 	// Dname is relative to table:Domains:name
 	Dname string
 }
+
+// ResolvedIPDIP stores table:Resolved_ips with domains, ips
+type ResolvedIPDIP struct {
+	tableName struct{} `sql:"resolved_ips,alias:resolved_ip"`
+	ID        uint
+	FirstSeen time.Time
+	LastSeen  time.Time
+
+	// relations
+	// Dname is relative to table:Domains:name
+	Dname string
+	// IP is realtive to table:IPs:ip
+	Ip string
+	// Type is realtive to table:IPs:ip_type
+	Type string
+}
