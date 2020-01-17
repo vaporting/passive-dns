1fefe1f7c6c608e3a231375b8f822447cd2a0d73
vaporting <freehotday@gmail.com>
2020-01-17 17:09:27 +0800

    add model: ResovledDomainDD for joining domain and resolved domain(domain)

--- models/resolved_domain.go
+++ models/resolved_domain.go
@@ -11,3 +11,17 @@ type ResolvedDomain struct {
 	FirstSeen        time.Time
 	LastSeen         time.Time
 }
+
+// ResolvedDomainDD stores table:Resolved_domains with domains, cname(resolved_domain)
+type ResolvedDomainDD struct {
+	tableName struct{} `sql:"resolved_domains,alias:resolved_domain"`
+	ID        uint
+	FirstSeen time.Time
+	LastSeen  time.Time
+
+	// relations
+	// Dname is relative to table:Domains:name
+	Dname string
+	// Cname is realtive to table:Domains:name
+	Cname string
+}
