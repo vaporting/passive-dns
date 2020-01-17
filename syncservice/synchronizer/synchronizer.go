8d3c7c3e2df2b3c80eff0b11f87ef78f5f31cbe5
vaporting <freehotday@gmail.com>
2020-01-17 14:57:01 +0800

    add createSyncers to create each synchronizer for sync service uses

--- syncservice/synchronizer/synchronizer.go
+++ syncservice/synchronizer/synchronizer.go
@@ -1,8 +1,18 @@
 package synchronizer
 
+import "passive-dns/types"
+
 // ISynchronizer is an interface of synchronizer
 type ISynchronizer interface {
 	Sync()
 	extract()
 	insert()
+}
+
+// CreateSyncers creates synchronizers
+func CreateSyncers(config *types.Config) []ISynchronizer {
+	syncers := []ISynchronizer{}
+	rIPSyncer, _ := NewResolvedIPSynchronizer(config)
+	syncers = append(syncers, &rIPSyncer)
+	return syncers
 }
